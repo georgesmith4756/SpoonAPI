99f1366c1c4c197d494814fe3d2cda104433d4a1
george smith <georgesmith4756@hotmail.co.uk>
2019-09-15 18:55:10 +0100

    Added pub list option

---
+++ src/main/java/com/qa/controllers/PubsController.java
@@ -0,0 +1,37 @@
+package com.qa.controllers;
+
+import com.qa.repository.PubsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.qa.models.Pub;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class PubsController {
+    @Autowired
+    private PubsRepository repository;
+
+    @RequestMapping(value = "publist", method = RequestMethod.GET)
+    public List<Pub> listAllPubs() {
+        return repository.findAll();
+    }
+
+    @RequestMapping(value = "publist", method = RequestMethod.POST)
+    public Pub addPub(@RequestBody Pub pub) {
+        return repository.saveAndFlush(pub);
+    }
+
+    @RequestMapping(value = "publist/", method = RequestMethod.GET)
+    public Pub getPub(@PathVariable Long id) {
+        return repository.findOne(id);
+    }
+
+    @RequestMapping(value = "publist/{id}", method = RequestMethod.DELETE)
+    public Pub deletePub(@PathVariable Long id) {
+        Pub existing = repository.findOne(id);
+        repository.delete(existing);
+        return existing;
+    }
+}
