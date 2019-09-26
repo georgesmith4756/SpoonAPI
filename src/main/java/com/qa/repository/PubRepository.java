ce068bf9db95480237ff78f3d3832a8761eb2830
george smith <georgesmith4756@hotmail.co.uk>
2019-09-26 14:52:34 +0100

    replaced ?1

--- src/main/java/com/qa/repository/PubRepository.java
+++ src/main/java/com/qa/repository/PubRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface PubRepository extends JpaRepository<Pubs, Long> {
 
-    @Query(value = "SELECT COUNT(DISTINCT pub) FROM entries WHERE username = ?1", nativeQuery = true)
+    @Query(value = "SELECT COUNT(DISTINCT pub) FROM entries WHERE username = :username", nativeQuery = true)
     Pubs findByUsername(@Param("username") String username);
 
 }
