7f96e56540aaa5e72fe4beadbc68dc018ecc2aca
george smith <georgesmith4756@hotmail.co.uk>
2019-09-26 14:12:03 +0100

    added pub counter

--- src/main/java/com/qa/controllers/PubController.java
+++ src/main/java/com/qa/controllers/PubController.java
@@ -2,6 +2,7 @@ package com.qa.controllers;
 
 import com.qa.repository.PubsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.*;
 import com.qa.models.Pub;
 
@@ -54,12 +55,16 @@ public class PubController {
         return repository.count();
     }
 
-    @RequestMapping(value = "getunique/{username}", method = RequestMethod.GET)
+
+
+    @RequestMapping(value = "getuniquepubs", method = RequestMethod.GET)
     public Long findByUsername(@PathVariable String username) {
         return repository.count();
     }
 
-
-
-
 }
+
+
+
+
+
