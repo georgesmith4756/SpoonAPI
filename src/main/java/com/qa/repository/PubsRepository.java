a12e4a5e17e97da286b93126407b583170b9c2fe
george smith <georgesmith4756@hotmail.co.uk>
2019-09-26 15:07:55 +0100

    altered parameter name

--- src/main/java/com/qa/repository/PubsRepository.java
+++ src/main/java/com/qa/repository/PubsRepository.java
@@ -3,11 +3,12 @@ package com.qa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.qa.models.Pub;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PubsRepository extends JpaRepository<Pub, Long> {
 
     @Query(value = "SELECT COUNT(DISTINCT pub) FROM entries WHERE username = :username", nativeQuery = true)
-    Long findByUsername(String username);
+    Long findByUsername(@Param("username") String username);
 
 
 }
